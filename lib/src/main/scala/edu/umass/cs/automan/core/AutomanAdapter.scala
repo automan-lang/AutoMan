b87663b029b5c305ebacf9484f9552f36aa3babd
Dan Barowy <dbarowy@cs.umass.edu>
2014-01-06 15:51:43 -0500

    Adapter refactoring for actors.

--- lib/src/main/scala/edu/umass/cs/automan/core/AutomanAdapter.scala
+++ lib/src/main/scala/edu/umass/cs/automan/core/AutomanAdapter.scala
@@ -9,51 +9,20 @@ import memoizer.{ThunkLogger, AutomanMemoizer}
 import scheduler.Thunk
 import strategy._
 
-abstract class AutomanAdapter[RBQ <: RadioButtonQuestion,
-                              CBQ <: CheckboxQuestion,
-                              FTQ <: FreeTextQuestion] {
+abstract class AdapterState {
   protected var _budget: BigDecimal = 0.00
   protected var _confidence: Double = 0.95
-  protected var _locale: Locale = Locale.getDefault
   protected var _strategy: Class[_ <: ValidationStrategy] = classOf[DefaultStrategy]
-  protected var _memoizer: AutomanMemoizer = _
-  protected var _memo_conn_string: String = "jdbc:derby:AutomanMemoDB;create=true"
-  protected var _memo_user: String = ""
-  protected var _memo_pass: String = ""
-  protected var _thunklog: ThunkLogger = _
-  protected var _thunk_conn_string: String = "jdbc:derby:ThunkLogDB;create=true"
-  protected var _thunk_user: String = ""
-  protected var _thunk_pass: String = ""
+  protected var _locale: Locale = Locale.getDefault
 
-  // getters and setters
-  def accept(t: Thunk)
   def budget: BigDecimal = _budget
   def budget_=(b: BigDecimal) { _budget = b }
-  def cancel(t: Thunk)
   def confidence: Double = _confidence
   def confidence_=(c: Double) { _confidence = c }
-  def memo_init() {
-    _memoizer = new AutomanMemoizer(_memo_conn_string, _memo_user, _memo_pass)
-  }
-  def thunklog_init() {
-    _thunklog = new ThunkLogger(_thunk_conn_string, _thunk_user, _thunk_pass)
-  }
-  def post(ts: List[Thunk], dual: Boolean, exclude_worker_ids: List[String])
-  def process_custom_info(t: Thunk, i: Option[String])
-  def reject(t: Thunk)
-  def retrieve(ts: List[Thunk]) : List[Thunk]  // returns all thunks passed in
+  def locale: Locale = _locale
+  def locale_=(l: Locale) { _locale = l }
   def strategy = _strategy
   def strategy_=(s: Class[ValidationStrategy]) { _strategy = s }
-
-  // Question creation
-  def CheckboxQuestion(fq: CBQ => Unit) : Future[CheckboxAnswer]
-  def FreeTextQuestion(fq: FTQ => Unit) : Future[FreeTextAnswer]
-  def RadioButtonQuestion(fq: RBQ => Unit) : Future[RadioButtonAnswer]
-  
-  // Option creation
-  def Option(id: Symbol, text: String) : QuestionOption
-
-  // Global backend config
   def budget_formatted = {
     val dbudget = _budget.setScale(2, BigDecimal.RoundingMode.HALF_EVEN)
     val nf = NumberFormat.getCurrencyInstance(_locale)
@@ -61,10 +30,40 @@ abstract class AutomanAdapter[RBQ <: RadioButtonQuestion,
     nf.setMaximumFractionDigits(2)
     nf.format(dbudget.doubleValue())
   }
-  def get_budget_from_backend(): BigDecimal
-  def locale: Locale = _locale
-  def locale_=(l: Locale) { _locale = l }
-  def schedule(q: RBQ): Future[RadioButtonAnswer]
-  def schedule(q: CBQ): Future[CheckboxAnswer]
-  def schedule(q: FTQ): Future[FreeTextAnswer]
+}
+
+trait AutomanAdapter[AS <: AdapterState,
+                     RBQ <: RadioButtonQuestion,
+                     CBQ <: CheckboxQuestion,
+                     FTQ <: FreeTextQuestion] {
+  protected val _memo_conn_string: String = "jdbc:derby:AutomanMemoDB;create=true"
+  protected val _memo_user: String = ""
+  protected val _memo_pass: String = ""
+  protected val _thunk_conn_string: String = "jdbc:derby:ThunkLogDB;create=true"
+  protected val _thunk_user: String = ""
+  protected val _thunk_pass: String = ""
+
+  // deferred because they depend on the above
+  protected val _memoizer: AutomanMemoizer = new AutomanMemoizer(_memo_conn_string, _memo_user, _memo_pass)
+  protected val _thunklog: ThunkLogger = new ThunkLogger(_thunk_conn_string, _thunk_user, _thunk_pass)
+
+  // Scheduler control functions
+  def accept(t: Thunk, a: AS)
+  def cancel(t: Thunk, a: AS)
+  def post(ts: List[Thunk], dual: Boolean, exclude_worker_ids: List[String], a: AS)
+  def process_custom_info(t: Thunk, i: Option[String], a: AS)
+  def reject(t: Thunk, a: AS)
+  def retrieve(ts: List[Thunk], a: AS) : List[Thunk]  // must return all thunks passed in
+  def schedule(q: RBQ, a: AS): Future[RadioButtonAnswer]
+  def schedule(q: CBQ, a: AS): Future[CheckboxAnswer]
+  def schedule(q: FTQ, a: AS): Future[FreeTextAnswer]
+
+  // DSL
+  def CheckboxQuestion(fq: CBQ => Unit) : Future[CheckboxAnswer]
+  def FreeTextQuestion(fq: FTQ => Unit) : Future[FreeTextAnswer]
+  def RadioButtonQuestion(fq: RBQ => Unit) : Future[RadioButtonAnswer]
+  def Option(id: Symbol, text: String) : QuestionOption
+
+//  // Global backend config
+//  def get_budget_from_backend(): BigDecimal
 }
