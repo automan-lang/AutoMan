32606b066d53d36b9c7e3d7efde3e7f69ad08f40
Daniel W. Barowy <dbarowy@users.noreply.github.com>
2014-04-18 11:03:12 -0400

    Added a default SBT project.

--- project/Build.scala
+++ project/Build.scala
@@ -2,10 +2,8 @@ import sbt._
 import Keys._
 
 object HelloBuild extends Build {
-    // lazy val root = Project(id = "all",
-    //                         base = file("."))
-    // 										aggregate(automan, simple_program)
-    // 										dependsOn(automan, simple_program)
+    lazy val root = Project(id = "all",
+                            base = file(".")) aggregate(automan, simple_program)
 
     lazy val automan = Project(id = "automan",
                            		 base = file("libautoman"))
