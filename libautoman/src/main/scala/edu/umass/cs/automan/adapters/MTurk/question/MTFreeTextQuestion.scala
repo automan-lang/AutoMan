5018aa52734b2b9df9299708c5a0d63749cac2b1
Daniel Barowy <dbarowy@cs.umass.edu>
2014-06-10 15:03:47 -0400

    MTurkAdapter now utilizes a connection pool.

--- libautoman/src/main/scala/edu/umass/cs/automan/adapters/MTurk/question/MTFreeTextQuestion.scala
+++ libautoman/src/main/scala/edu/umass/cs/automan/adapters/MTurk/question/MTFreeTextQuestion.scala
@@ -91,7 +91,7 @@ class MTFreeTextQuestion extends FreeTextQuestion with MTurkQuestion {
           {
             // if formatted content is specified, use that instead of text field
             _formatted_content match {
-              case Some(x) => scala.xml.PCData(x.toString)
+              case Some(x) => <FormattedContent>{ scala.xml.PCData(x.toString) }</FormattedContent>
               case None => <Text>{ text }</Text>
             }
           }
