2f455cacb73321ea9468463cc902d2add30b2d4d
Daniel W. Barowy <dbarowy@users.noreply.github.com>
2014-06-18 15:52:09 -0400

    Added SimpleSurvey test app and implemented two missing distribution question methods.

---
+++ apps/SimpleSurvey/src/main/scala/SimpleSurvey.scala
@@ -0,0 +1,37 @@
+import edu.umass.cs.automan.adapters.MTurk._
+import edu.umass.cs.automan.core.Utilities
+import edu.umass.cs.automan.core.exception.OverBudgetException
+import scala.concurrent._
+import scala.concurrent.duration._
+
+object SimpleSurvey extends App {
+  val opts = Utilities.unsafe_optparse(args, "SimpleSurvey.jar")
+
+  val a = MTurkAdapter { mt =>
+    mt.access_key_id = opts('key)
+    mt.secret_access_key = opts('secret)
+    mt.sandbox_mode = opts('sandbox).toBoolean
+  }
+
+  def which_one(text: String) = a.RadioButtonDistributionQuestion { q =>
+    q.num_samples = 60
+    q.budget = 8.00
+    q.text = text
+    q.options = List(
+      a.Option('oscar, "Oscar the Grouch"),
+      a.Option('kermit, "Kermit the Frog"),
+      a.Option('spongebob, "Spongebob Squarepants"),
+      a.Option('cookie, "Cookie Monster"),
+      a.Option('count, "The Count")
+    )
+  }
+
+  try {
+    val future_answer = which_one("Which one of these does not belong?")
+    val answer = Await.result(future_answer, Duration.Inf)
+    println("answer1 is a " + answer)
+
+  } catch {
+    case OverBudgetException(e) => println("Over budget!")
+  }
+}
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,7 +1,5 @@
 addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4")
 
-//addSbtPlugin("org.scala-sbt.plugins" % "sbt-onejar" % "0.8")
-
 addSbtPlugin("org.xerial.sbt" % "sbt-pack" % "0.5.1")
 
 
