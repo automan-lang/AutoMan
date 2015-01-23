1202dd2f119e22bd586b4cdae8a620eece9926e9
Daniel W. Barowy <dbarowy@users.noreply.github.com>
2015-01-23 13:10:04 -0500

    Now that the user is required to indicate the scope of an AutoMan program, we can just shutdown the MTurk connection pool on demand.

--- libautoman/src/main/scala/edu/umass/cs/automan/automan.scala
+++ libautoman/src/main/scala/edu/umass/cs/automan/automan.scala
@@ -4,7 +4,6 @@ import edu.umass.cs.automan.core.AutomanAdapter
 
 object automan {
   def apply[T](a: AutomanAdapter)(block: => T): T = {
-//    a.init()    Adapters are initialized upon declaration, for now
     val output = block
     a.close()
     output
