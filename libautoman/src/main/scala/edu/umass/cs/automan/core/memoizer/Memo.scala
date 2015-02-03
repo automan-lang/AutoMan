d80a80c5c3af1c1370d2b5db9e3bab77f4d87cea
Daniel Barowy <dbarowy@cs.umass.edu>
2015-02-02 21:30:49 -0500

    save; too tired to think

--- libautoman/src/main/scala/edu/umass/cs/automan/core/memoizer/Memo.scala
+++ libautoman/src/main/scala/edu/umass/cs/automan/core/memoizer/Memo.scala
@@ -1,11 +1,10 @@
 package edu.umass.cs.automan.core.memoizer
 
-import java.util.Date
-
+import java.util.UUID
 import edu.umass.cs.automan.core.scheduler.SchedulerState
-
 import scala.slick.driver.DerbyDriver.simple._
 import scala.slick.jdbc.meta.MTable
+import scala.slick.lifted
 
 class Memo {
   // connection string
@@ -21,6 +20,10 @@ class Memo {
   // get DB handle
   private val db = Database.forURL(jdbc_conn_string, driver = "scala.slick.driver.DerbyDriver")
 
+  // Thunk existence cache
+  private val all_thunk_ids = scala.collection.mutable.Set[UUID]()
+
+  // create DB if it does not already exist
   db.withSession {
     implicit session =>
 
@@ -31,25 +34,33 @@ class Memo {
         question_thunk.ddl ++
         radio_button_answer.ddl
       ).create
+    } else {
+      // add all of the thunk_ids from the database to the tracked set
+      thunk.map(_.thunk_id).foreach(all_thunk_ids.add(_))
     }
   }
 
-  def saveNewThunk(t: edu.umass.cs.automan.core.scheduler.Thunk[_]) : Unit = {
-    db.withTransaction {
-      implicit session =>
-        assert(t.state == SchedulerState.READY)
+  def saveThunk(t: edu.umass.cs.automan.core.scheduler.Thunk[_]) : Unit = {
+    // if the Thunk already exists, just update the ThunkHistory table
+    if (all_thunk_ids.contains(t.thunk_id)) {
+      assert(t.state != SchedulerState.READY)
 
-        thunk += (t.thunk_id, t.computation_id, t.cost, t.created_at, t.question.id, t.timeout_in_s, t.worker_timeout)
-        thunk_history += (t.thunk_id, t.created_at, t.state, t.worker_id)
-    }
-  }
+      db.withTransaction {
+        implicit session =>
 
-  def updateThunk(t: edu.umass.cs.automan.core.scheduler.Thunk[_]) : Unit = {
-    db.withTransaction {
-      implicit session =>
-        assert(t.state != SchedulerState.READY)
+          thunk_history += (t.thunk_id, t.created_at, t.state, t.worker_id)
+      }
+    } else {
+      assert(t.state == SchedulerState.READY)
 
-        thunk_history += (t.thunk_id, new Date(), t.state, t.worker_id)
+      // otherwise, create a Thunk entry
+      db.withTransaction {
+        implicit session =>
+
+          thunk += (t.thunk_id, t.computation_id, t.cost, t.created_at, t.question.id, t.timeout_in_s, t.worker_timeout)
+          thunk_history += (t.thunk_id, t.created_at, t.state, t.worker_id)
+      }
+      all_thunk_ids.add(t.thunk_id)
     }
   }
 }
