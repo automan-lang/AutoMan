6d9f54ec947ff48d05dd1e5b20646f7608c010d4
Daniel W. Barowy <dbarowy@users.noreply.github.com>
2015-02-23 18:30:18 -0500

    take advantage of thread sleep time to update Memo DB

---
+++ libautoman/src/main/scala/edu/umass/cs/automan/core/scheduler/Stopwatch.scala
@@ -0,0 +1,12 @@
+package edu.umass.cs.automan.core.scheduler
+
+case class Time[A](result: A, duration_ms: Long)
+
+object Stopwatch {
+  def apply[A](fn: => A) : Time[A] = {
+    val start_time = System.currentTimeMillis()
+    val result = fn
+    val end_time = System.currentTimeMillis()
+    Time(result, end_time - start_time)
+  }
+}
