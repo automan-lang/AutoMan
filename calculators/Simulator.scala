a3b781047265dca7db923d3ca430e938dac38025
Dan Barowy <dbarowy@cs.umass.edu>
2015-04-29 14:46:17 -0400

    simulator now lets you compute the probability of the number of agreements.

--- calculators/Simulator.scala
+++ calculators/Simulator.scala
@@ -1,43 +1,71 @@
 import scala.util.Random
 
 object Simulator {
-  val LotsOfIterations: Int = 100000
+  val NumberOfSimulations: Int = 100000
 
-  def RequiredForAgreement(options: Int, trials: Int, confidence: Double, iterations: Int) : Option[Int] = {
+  def GetMinTrialsForAgreement(options: Int, confidence: Double, simulations: Int) : Int = {
+    var done = false
+    var trials = 1
+    while(!done) {
+      RequiredForAgreement(options, trials, confidence, simulations) match {
+        case Some(t) => done = true
+        case None => trials += 1
+      }
+    }
+    trials
+  }
+
+  def RequiredForAgreement(choices: Int, trials: Int, confidence: Double, simulations: Int) : Option[Int] = {
+    val outcomes = SimulateNTimes(choices, trials, simulations)
+
+    CalculateMinTrialsForAgreement(trials, confidence, simulations, outcomes)
+  }
+
+  def SimulateNTimes(choices: Int, trials: Int, simulations: Int) : Array[Int] = {
     val rng = new Random()
-    val occurrences = new Array[Int](trials + 1)
+    val outcomes = new Array[Int](trials + 1)
 
     // run Monte Carlo simulation
-    for (i <- 0 until iterations) {
-      Iterate(options, trials, occurrences, rng)
+    for (i <- 0 until simulations) {
+      // simulate flipping a (1/choices) coin for (trials) trials and return the size of the winning bin
+      // which must always be <= the number of trials
+      val max_agreement = Simulate(choices, trials, rng)
+      // update max_agreement count
+      outcomes(max_agreement) += 1
     }
-
-    CalculateMinTrialsForAgreement(trials, confidence, iterations, occurrences)
+    
+    outcomes
   }
 
-  def Iterate(options: Int, trials: Int, occurrences: Array[Int], r: Random) : Unit = {
-    val counter = new Array[Int](options + 1)
+  def Simulate(choices: Int, trials: Int, r: Random) : Int = {
+    val histogram = new Array[Int](choices)
 
-    // randomly select one of k options for j trials
+    // randomly select one of k choices for j trials
     for (j <- 0 until trials) {
-      counter(r.nextInt(options)) += 1
+      histogram(r.nextInt(choices)) += 1
     }
 
-    // save the size of the largest bin
-    val binidx = counter.reduce((biggest, bin) => Math.max(biggest, bin))
-    occurrences(binidx) += 1
+    // return the size of the biggest bin
+    histogram.reduce((biggest, bin) => Math.max(biggest, bin))
   }
 
-  def CalculateMinTrialsForAgreement(trials: Int, confidence: Double, iterations: Int, occurrences: Array[Int]) : Option[Int] = {
+  def CalculateProbability(choices: Int, trials: Int, num_agree: Int, simulations: Int) : Double = {
+    val outcomes = SimulateNTimes(choices, trials, simulations)
+    
+    // return the probability that (num_agree) agreements occur
+    outcomes(num_agree).toDouble / simulations.toDouble
+  }
+
+  def CalculateMinTrialsForAgreement(trials: Int, confidence: Double, simulations: Int, outcomes: Array[Int]) : Option[Int] = {
     var i = 1
     var p = 1.0
     val alpha = 1.0 - confidence
     while (i <= trials && p > alpha) {
-      var p_i = occurrences(i).toDouble / iterations.toDouble
+      var p_i = outcomes(i).toDouble / simulations.toDouble
       p -= p_i
       i += 1
     }
-
+  
     if (i <= trials && p <= alpha) {
       Some(i)
     } else {
@@ -45,13 +73,12 @@ object Simulator {
     }
   }
 
-  // how many trials must I stack up to wash you out of my mind, out of my consciousness?
   def HowManyMoreTrials(existing_trials: Int, agreement: Int, options: Int, confidence: Double) : Int = {
     var num_addl_trials = 0
     var done = false
     while (!done) {
       val expected = agreement + num_addl_trials
-      val min_required = RequiredForAgreement(options, existing_trials + num_addl_trials, confidence, LotsOfIterations)
+      val min_required = RequiredForAgreement(options, existing_trials + num_addl_trials, confidence, NumberOfSimulations)
       min_required match {
         case Some(min_req) =>
           if (min_req > expected) {
@@ -67,26 +94,30 @@ object Simulator {
   }
 
   def invoked_as_name : String = {
-//    Simulator.getClass.getProtectionDomain.getCodeSource.getLocation.getPath.split("/").last
     "Simulator"
   }
 
   def usage() : Unit = {
     val usage = "Usage: \n" +
-      invoked_as_name + " -r [num options] [num trials] [confidence]\n" +
-      " or\n" +
-      invoked_as_name + " -m [num trials already run] [num that agree] [num options] [confidence]"
+      "To determine the minimum number of trials to run for a given number of choices and confidence:\n" +
+      "\t" + invoked_as_name + " -r [num choices] [confidence]\n" +
+      "or\n" +
+      "To determine how many more trials to run for a given number of agreements, choices, and confidence:\n" +
+      "\t" + invoked_as_name + " -m [num trials already run] [num that agree] [num choices] [confidence]\n" +
+      "or\n" +
+      "To determine the (approximate) probability of having a given number of agreements for some number of choices and trials:\n" +
+      "\t" + invoked_as_name + " -p [num choices] [num trials] [num that agree]"
     println(usage)
   }
 
-  def validate_numopts(num_opts: String) : Int = {
-    val num_opts_i = num_opts.toInt
-    if (num_opts_i < 2) {
-      println("ERROR: num_opts must be at least 2.")
+  def validate_numchoices(num_choices: String) : Int = {
+    val num_choices_i = num_choices.toInt
+    if (num_choices_i < 2) {
+      println("ERROR: num_choices must be at least 2.")
       usage()
       sys.exit(1)
     } else {
-      num_opts_i
+      num_choices_i
     }
   }
   
@@ -134,23 +165,22 @@ object Simulator {
     }
   }
 
-  def parseOptions(arglist: List[String]) : () => Option[Int] = {
-    if (arglist.length < 4 || arglist.length > 5) {
-      usage()
-      sys.exit(1)
-    }
-
+  def parseOptions(arglist: List[String]) = {
     arglist match {
-      case "-r" :: num_opts :: num_trials :: confidence :: _ =>
-        () => RequiredForAgreement(validate_numopts(num_opts),
-                                   validate_numtrials(num_trials),
-                                   validate_confidence(confidence),
-                                   LotsOfIterations)
-      case "-m" :: num_run :: num_agree :: num_opts :: confidence :: _ =>
-        () => Some(HowManyMoreTrials(validate_numrun(num_run),
-                                     validate_numagree(num_agree),
-                                     validate_numopts(num_opts),
-                                     validate_confidence(confidence)))
+      case "-r" :: num_choices :: confidence :: _ =>
+        () => GetMinTrialsForAgreement(validate_numchoices(num_choices),
+                                       validate_confidence(confidence),
+                                       NumberOfSimulations)
+      case "-m" :: num_run :: num_agree :: num_choices :: confidence :: _ =>
+        () => HowManyMoreTrials(validate_numrun(num_run),
+                                validate_numagree(num_agree),
+                                validate_numchoices(num_choices),
+                                validate_confidence(confidence))
+      case "-p" :: num_choices :: num_trials :: num_agree :: _ =>
+        () => CalculateProbability(validate_numchoices(num_choices),
+                                        validate_numtrials(num_trials),
+                                        validate_numagree(num_agree),
+                                        NumberOfSimulations)
       case _ => {
         usage()
         sys.exit(1)
@@ -161,9 +191,6 @@ object Simulator {
   def main(args: Array[String]): Unit = {
     val fn = parseOptions(args.toList)
 
-    fn() match {
-      case Some(i) => println(i)
-      case None => println("Insufficent number of trials to establish confidence.")
-    }
+    println(fn())
   }
 }
