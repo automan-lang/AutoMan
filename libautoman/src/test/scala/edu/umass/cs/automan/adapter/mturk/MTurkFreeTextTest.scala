711881c759f2f7283a0eeabd034cba78678838ed
Daniel W. Barowy <dbarowy@users.noreply.github.com>
2015-05-08 16:23:05 -0400

    Ensure that default QC algorithm always schedules at least two tasks.

--- libautoman/src/test/scala/edu/umass/cs/automan/adapter/mturk/MTurkFreeTextTest.scala
+++ libautoman/src/test/scala/edu/umass/cs/automan/adapter/mturk/MTurkFreeTextTest.scala
@@ -24,7 +24,7 @@ class MTurkFreeTextTest extends FlatSpec with Matchers {
         q.budget = 8.00
         q.text = "Which 4-letter metasyntactic variable starts with 'q'?"
         q.pattern = "AAAA"
-        q.mock_answers = List("quux","foo","bar","norf","quux")
+        q.mock_answers = List("quux","foo","bar","norf","quux","quux")
       }
 
       which_one().answer match {
