e1d5a0995e995ebb879ce31061c4c2a8e5c0b59a
Daniel W. Barowy <dbarowy@users.noreply.github.com>
2015-06-04 08:30:40 -0400

    include question type with TaskSnapshot

--- libautoman/src/main/scala/edu/umass/cs/automan/core/logging/TaskSnapshot.scala
+++ libautoman/src/main/scala/edu/umass/cs/automan/core/logging/TaskSnapshot.scala
@@ -2,6 +2,7 @@ package edu.umass.cs.automan.core.logging
 
 import java.util.{Date, UUID}
 
+import edu.umass.cs.automan.core.info.QuestionType.QuestionType
 import edu.umass.cs.automan.core.scheduler.SchedulerState
 
 case class TaskSnapshot[T](task_id: UUID,
@@ -16,7 +17,8 @@ case class TaskSnapshot[T](task_id: UUID,
                            state: SchedulerState.Value,
                            worker_id: Option[String],
                            answer: Option[T],
-                           state_changed_at: Date) {
-  def this(tup: (UUID,UUID,String,String,Int,Int,Int,BigDecimal,Date,SchedulerState.Value,Option[String],Option[T],Date)) =
-  this(tup._1,tup._2,tup._3,tup._4,tup._5,tup._6,tup._7,tup._8,tup._9,tup._10,tup._11,tup._12,tup._13)
+                           state_changed_at: Date,
+                           question_type: QuestionType ) {
+  def this(tup: (UUID,UUID,String,String,Int,Int,Int,BigDecimal,Date,SchedulerState.Value,Option[String],Option[T],Date,QuestionType)) =
+  this(tup._1,tup._2,tup._3,tup._4,tup._5,tup._6,tup._7,tup._8,tup._9,tup._10,tup._11,tup._12,tup._13,tup._14)
 }
