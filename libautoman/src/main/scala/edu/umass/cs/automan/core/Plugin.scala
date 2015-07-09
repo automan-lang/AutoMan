08ec7103c17a9cf8692fe7d2f582e98bd724cd86
Daniel W. Barowy <dbarowy@users.noreply.github.com>
2015-07-09 10:20:33 -0400

    Plugins are now notified of state changes.

--- libautoman/src/main/scala/edu/umass/cs/automan/core/Plugin.scala
+++ libautoman/src/main/scala/edu/umass/cs/automan/core/Plugin.scala
@@ -1,6 +1,9 @@
 package edu.umass.cs.automan.core
 
+import edu.umass.cs.automan.core.logging.TaskSnapshot
+
 trait Plugin {
   def startup(adapter: AutomanAdapter)
   def shutdown()
+  def state_updates(tasks: List[TaskSnapshot[_]])
 }
