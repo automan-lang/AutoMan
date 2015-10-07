e4558e346331b09d9b91d4b6e6dedf3e00de4eb4
dbarowy <dbarowy@users.noreply.github.com>
2015-10-07 17:00:16 -0400

    Update banana_question app.

--- apps/banana_question/src/main/scala/banana_question.scala
+++ apps/banana_question/src/main/scala/banana_question.scala
@@ -1,37 +1,51 @@
-//import edu.umass.cs.automan.adapters.MTurk._
-//import edu.umass.cs.automan.core.Utilities
-//import edu.umass.cs.automan.core.exception.OverBudgetException
-//import scala.concurrent._
-//import scala.concurrent.duration._
-//
-//object banana_question extends App {
-//  val opts = Utilities.unsafe_optparse(args, "banana_question.jar")
-//
-//  val a = MTurkAdapter { mt =>
-//    mt.access_key_id = opts('key)
-//    mt.secret_access_key = opts('secret)
-//    mt.sandbox_mode = opts('sandbox).toBoolean
-//  }
-//
-//  def which_one(text: String) = a.RadioButtonQuestion { q =>
-//    q.budget = 5.00
-//    q.text = text
-//    q.options = List(
-//      a.Option('banana, "Banana", "http://tinyurl.com/7u5d4n8"),
-//      a.Option('apple, "Apple", "http://tinyurl.com/7egjghy"),
-//      a.Option('celery, "Celery", "http://tinyurl.com/7df3z6s"),
-//      a.Option('cucumber, "cucumber", "http://tinyurl.com/77bgukv"),
-//      a.Option('orange, "orange", "http://tinyurl.com/77y7o57")
-//    )
-//    q.question_timeout_multiplier = 20
-//  }
-//
-//  try {
-//    val future_answer = which_one("Which one of these does not belong?")
-//    val answer = Await.result(future_answer, Duration.Inf)
-//    println("answer1 is a " + answer)
-//
-//  } catch {
-//    case OverBudgetException(e) => println("Over budget!")
-//  }
-//}
+import edu.umass.cs.automan.adapters.mturk._
+import edu.umass.cs.automan.adapters.mturk.question.MTQuestionOption
+import java.util.UUID
+import java.net.URL
+import javax.imageio._
+import bananalib._
+
+object banana_question extends App {
+  val opts = my_optparse(args, "banana_question.jar")
+
+  val bucketname = ("bananaquestion" + UUID.randomUUID().toString).toLowerCase
+
+  val a = MTurkAdapter { mt =>
+    mt.access_key_id = opts('key)
+    mt.secret_access_key = opts('secret)
+    mt.sandbox_mode = opts('sandbox).toBoolean
+  }
+
+  def which_one(text: String, options: List[MTQuestionOption]) = a.RadioButtonQuestion { q =>
+    q.budget = 5.00
+    q.text = text
+    q.options = options
+  }
+  
+  // images:
+  val images = Map(
+    "banana" -> "http://tinyurl.com/qej5zad",
+    "apple" -> "http://tinyurl.com/qxn58kn",
+    "celery" -> "http://tinyurl.com/q38566j",
+    "cucumber" -> "http://tinyurl.com/ps6bgg3",
+    "orange" -> "http://tinyurl.com/ntxzjjt"
+  )
+  
+  // download and resize each image
+  val scaled = images.map { case (name,url) => name -> resize(ImageIO.read(new URL(url)), 150) }
+
+  // store each image in S3
+  val s3client = init_s3(opts('key), opts('secret), bucketname)
+  val s3_urls = scaled.map{ case (name,img) => name -> getTinyURL(store_in_s3(img, s3client, bucketname)) }
+  
+  // create options
+  val banana_opts = s3_urls.map { case (name, url) => a.Option(Symbol(name), name, url) }.toList
+
+  automan(a) {
+    val outcome = which_one("Which one of these does not belong?", banana_opts)
+    outcome.answer match {
+      case Answer(answer,_,_) => println("The answer is: " + answer)
+      case _ => println("An error occurred.")
+    }
+  }
+}
