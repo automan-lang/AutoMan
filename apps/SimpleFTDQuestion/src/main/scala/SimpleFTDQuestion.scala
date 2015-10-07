9708f7cde8b75ddc12ecc59032c2b43e5161865c
dbarowy <dbarowy@users.noreply.github.com>
2015-10-07 19:45:03 -0400

    Update app.

--- apps/SimpleFTDQuestion/src/main/scala/SimpleFTDQuestion.scala
+++ apps/SimpleFTDQuestion/src/main/scala/SimpleFTDQuestion.scala
@@ -1,29 +1,32 @@
-//import edu.umass.cs.automan.adapters.MTurk.MTurkAdapter
-//import edu.umass.cs.automan.automan
-//import edu.umass.cs.automan.core.Utilities
-//import scala.concurrent.Await
-//import scala.concurrent.duration.Duration
+import edu.umass.cs.automan.adapters.mturk._
 
 object SimpleFTDQuestion extends App {
-//  val opts = Utilities.unsafe_optparse(args, "SimpleFTDQuestion.scala")
-//
-//  // init MTurk backend
-//  val a = MTurkAdapter { mt =>
-//    mt.access_key_id = opts('key)
-//    mt.secret_access_key = opts('secret)
-//    mt.sandbox_mode = opts('sandbox).toBoolean
-//  }
-//
-//  def AskIt(question: String) = a.FreeTextDistributionQuestion { q =>
-//    q.num_samples = 3
-//    q.title = question
-//    q.text = question
-//  }
-//
-//  val answers = automan(a) {
-//    val future_answers = AskIt("How many licks does it take to get to the Tootsie Roll Center of a Tootsie Pop?")
-//    Await.result(future_answers, Duration.Inf)
-//  }
-//
-//  answers.map { answer => println("Answer: " + answer.toString())}
+  val sample_size = 3
+  
+  val opts = Utilities.unsafe_optparse(args, "SimpleFTDQuestion.scala")
+
+  val a = MTurkAdapter { mt =>
+    mt.access_key_id = opts('key)
+    mt.secret_access_key = opts('secret)
+    mt.sandbox_mode = opts('sandbox).toBoolean
+  }
+
+  def AskIt(question: String) = a.FreeTextDistributionQuestion { q =>
+    q.sample_size = sample_size
+    q.title = question
+    q.text = question
+  }
+
+  automan(a) {
+    val outcome = AskIt("How many licks does it take to get to the Tootsie Roll Center of a Tootsie Pop?")
+
+    outcome.answer match {
+      case Answers(answers,_) =>
+        answers.map { case (worker_id, answer) => println("Worker ID: " + worker_id + ", Answer: " + answer.toString()) }
+      case IncompleteAnswers(answers,_) =>
+        println("Ran out of money!  Only have " + answers.size + " of " + sample_size + " responses.")
+        answers.map { case (worker_id, answer) => println("Worker ID: " + worker_id + ", Answer: " + answer.toString()) }
+      case OverBudgetAnswers(need,have) => println("Over budget.  Need: $" + need + ", have: $" + have)
+    }
+  }
 }
