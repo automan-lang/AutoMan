69ff6f707dd132fcafac8dd4f1a96090831e3973
dbarowy <dbarowy@users.noreply.github.com>
2016-02-13 12:37:44 -0500

    Add log statements for price policy calculation.

--- libautoman/src/main/scala/edu/umass/cs/automan/core/policy/price/MLEPricePolicy.scala
+++ libautoman/src/main/scala/edu/umass/cs/automan/core/policy/price/MLEPricePolicy.scala
@@ -1,12 +1,15 @@
 package edu.umass.cs.automan.core.policy.price
 
+import edu.umass.cs.automan.core.logging.{LogType, LogLevelDebug, DebugLog}
 import edu.umass.cs.automan.core.question.Question
 import edu.umass.cs.automan.core.scheduler.{SchedulerState, Task}
 
 class MLEPricePolicy(question: Question) extends PricePolicy(question) {
   def calculateReward(tasks: List[Task], round: Int, timeout_occurred: Boolean) : BigDecimal = {
-    if (round == 0 && tasks.size == 0) {
-      calculateInitialReward()
+    if (round == 0 && tasks.isEmpty) {
+      val reward = calculateInitialReward()
+      DebugLog(s"Initial reward is $$$reward.", LogLevelDebug(), LogType.STRATEGY, question.id)
+      reward
     } else {
       // find the last round where we spawned tasks
       val last_spawn_round = tasks.map(_.round).max
@@ -25,7 +28,15 @@ class MLEPricePolicy(question: Question) extends PricePolicy(question) {
         // use the MLE for the Bernoulli distribution (the mean) to
         // find the probability that a task will remain available (p_a)
         val p_a: BigDecimal = BigDecimal(num_unanswered) / BigDecimal(last_round.size)
-        (1.0 / p_a * current_reward).setScale(2, math.BigDecimal.RoundingMode.FLOOR)
+        val reward = (1.0 / p_a * current_reward).setScale(2, math.BigDecimal.RoundingMode.FLOOR)
+
+        DebugLog(s"Timeout occurred. New reward is $$$reward because the estimated acceptance " +
+                 s"rate is $p_a per round and the current reward is $$$current_reward.",
+                 LogLevelDebug(),
+                 LogType.STRATEGY,
+                 question.id)
+
+        reward
       } else {
         current_reward
       }
