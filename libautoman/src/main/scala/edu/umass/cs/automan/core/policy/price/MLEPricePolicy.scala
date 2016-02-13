d3373e44cff60c46d6bb932394bd493ce723519e
dbarowy <dbarowy@users.noreply.github.com>
2016-02-13 14:02:15 -0500

    Fix p_a

--- libautoman/src/main/scala/edu/umass/cs/automan/core/policy/price/MLEPricePolicy.scala
+++ libautoman/src/main/scala/edu/umass/cs/automan/core/policy/price/MLEPricePolicy.scala
@@ -5,30 +5,50 @@ import edu.umass.cs.automan.core.question.Question
 import edu.umass.cs.automan.core.scheduler.{SchedulerState, Task}
 
 class MLEPricePolicy(question: Question) extends PricePolicy(question) {
+  private def numAnswered(ts: List[Task]) : Int = {
+    ts.count { t =>
+      t.state == SchedulerState.ANSWERED ||
+      t.state == SchedulerState.ACCEPTED ||
+      t.state == SchedulerState.REJECTED ||
+      t.state == SchedulerState.DUPLICATE
+    }
+  }
+
+  private def numTimeouts(ts: List[Task]) : Int = {
+    // find the last round where we spawned tasks
+    val last_spawn_round = ts.map(_.round).max
+    // get the tasks from that round
+    val last_round = ts.filter(_.round == last_spawn_round)
+
+    // get # timed-out tasks
+    last_round.count { t =>
+      t.state == SchedulerState.TIMEOUT || t.state == SchedulerState.CANCELLED
+    }
+  }
+
   def calculateReward(tasks: List[Task], round: Int, timeout_occurred: Boolean) : BigDecimal = {
     if (round == 0 && tasks.isEmpty) {
       val reward = calculateInitialReward()
       DebugLog(s"Initial reward is $$$reward.", LogLevelDebug(), LogType.STRATEGY, question.id)
       reward
     } else {
-      // find the last round where we spawned tasks
-      val last_spawn_round = tasks.map(_.round).max
-      // get the tasks from that round
-      val last_round = tasks.filter(_.round == last_spawn_round)
       // if timeouts occur, a round will contain mixed prices; take the max
-      val current_reward = last_round.map(_.cost).max
+      val current_reward = tasks.map(_.cost).max
 
       if (timeout_occurred) {
-        // get # unanswered tasks
-        val num_unanswered = last_round.count(_.state == SchedulerState.TIMEOUT)
+        // # unanswered in last roundcannot be zero,
+        // otherwise a timeout would not have occurred
+        assert(numTimeouts(tasks) != 0)
 
-        // # unanswered cannot be zero, otherwise a timeout would not have occurred
-        assert(num_unanswered != 0)
+        val num_answered = numAnswered(tasks)
 
-        // use the MLE for the Bernoulli distribution (the mean) to
-        // find the probability that a task will remain available (p_a)
-        val p_a: BigDecimal = BigDecimal(num_unanswered) / BigDecimal(last_round.size)
-        val reward = (1.0 / p_a * current_reward).setScale(2, math.BigDecimal.RoundingMode.FLOOR)
+        // Use the MLE for the Bernoulli distribution (the mean) to
+        // find the probability that a task will be accepted (p_a).
+        // We assume that p_a is a fixed population parameter unaffected by price.
+        val p_a: BigDecimal = BigDecimal(num_answered) / BigDecimal(tasks.size)
+        // maximal safe growth rate; see CACM paper.
+        val growth_rate: BigDecimal = 1.0 / p_a
+        val reward = (growth_rate * current_reward).setScale(2, math.BigDecimal.RoundingMode.FLOOR)
 
         DebugLog(s"Timeout occurred. New reward is $$$reward because the estimated acceptance " +
                  s"rate is $p_a per round and the current reward is $$$current_reward.",
