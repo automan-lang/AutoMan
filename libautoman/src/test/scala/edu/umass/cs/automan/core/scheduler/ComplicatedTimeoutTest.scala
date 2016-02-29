f107300d6dceb6b37e2204ab5e4de8629adef217
dbarowy <dbarowy@users.noreply.github.com>
2016-02-29 14:18:35 -0500

    Specifically test for the given cost.

--- libautoman/src/test/scala/edu/umass/cs/automan/core/scheduler/ComplicatedTimeoutTest.scala
+++ libautoman/src/test/scala/edu/umass/cs/automan/core/scheduler/ComplicatedTimeoutTest.scala
@@ -56,7 +56,7 @@ class ComplicatedTimeoutTest extends FlatSpec with Matchers {
         case Answer(value, cost, conf, _) =>
           println("Answer: '" + value + "', cost: '" + cost + "', confidence: " + conf)
           (value == 'spongebob) should be (true)
-//          cost should be (BigDecimal(0.30))
+          cost should be (BigDecimal(2.10))
           (conf > 0.95) should be (true)
         case _ =>
           fail()
