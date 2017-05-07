14649d0c810b253b33ca1c8b5b41c5196af4d2fa
dbarowy <dbarowy@users.noreply.github.com>
2017-05-06 18:05:19 -0600

    A handy calculator I've always meant to write.

---
+++ tools/NumToRun/build.sbt
@@ -0,0 +1,11 @@
+name := "NumToRun"
+
+version := "1.0"
+
+organization := "edu.umass.cs"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  "edu.umass.cs"        %% "automan" % "1.2-SNAPSHOT"
+)
---
+++ tools/NumToRun/src/main/scala/NumToRun.scala
@@ -0,0 +1,22 @@
+import edu.umass.cs.automan.core.policy.aggregation.AgreementSimulation
+
+object NumToRun extends App {
+  val choices = args(0).toInt
+  var trials = args(1).toInt
+  var agree = args(2).toInt
+  var conf = 0.0
+  
+  val NUMSIMS = 10000
+  val target_conf = 0.95
+  
+  while (conf < target_conf) {
+    conf = AgreementSimulation.confidenceOfOutcome(choices, trials, agree, NUMSIMS)
+    
+    if (conf < target_conf) {
+      trials += 1
+      agree += 1
+    }
+  }
+  
+  println(trials + " are needed to answer a " + choices + "-option question where " + agree + " people initially agree.")
+}
