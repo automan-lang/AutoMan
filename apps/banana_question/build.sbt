a84b566d85a58764c4304982168e37cb8db591c3
dbarowy <dbarowy@users.noreply.github.com>
2017-05-08 19:07:29 -0600

    Update banana_question app

--- apps/banana_question/build.sbt
+++ apps/banana_question/build.sbt
@@ -4,7 +4,7 @@ packMain := Map("banana_question" -> "banana_question")
 
 name := "BananaQuestion"
 
-version := "0.2"
+version := "0.3"
 
 organization := "edu.umass.cs"
 
@@ -13,7 +13,7 @@ scalaVersion := "2.11.7"
 exportJars := true
 
 libraryDependencies ++= Seq(
-  "edu.umass.cs" %% "automan" % "1.1.6",
+  "edu.umass.cs" %% "automan" % "1.2-SNAPSHOT",
   "com.amazonaws"   %  "aws-java-sdk"	% "1.10.23",
   "org.imgscalr"    %  "imgscalr-lib"	% "4.2"
 )
--- apps/banana_question/src/main/scala/banana_question.scala
+++ apps/banana_question/src/main/scala/banana_question.scala
@@ -1,11 +1,8 @@
-import edu.umass.cs.automan.adapters.mturk._
-import edu.umass.cs.automan.adapters.mturk.question.MTQuestionOption
+import edu.umass.cs.automan.adapters.mturk.DSL._
 import java.util.UUID
 import java.net.URL
 import javax.imageio._
-
 import bananalib._
-import edu.umass.cs.automan.core.answer.Answer
 import edu.umass.cs.automan.core.policy.aggregation.UserDefinableSpawnPolicy
 
 object banana_question extends App {
@@ -13,18 +10,18 @@ object banana_question extends App {
 
   val bucketname = ("bananaquestion" + UUID.randomUUID().toString).toLowerCase
 
-  val a = MTurkAdapter { mt =>
-    mt.access_key_id = opts('key)
-    mt.secret_access_key = opts('secret)
-    mt.sandbox_mode = opts('sandbox).toBoolean
-  }
+  implicit val a = mturk (
+    access_key_id = opts('key),
+    secret_access_key = opts('secret),
+    sandbox_mode = opts('sandbox).toBoolean
+  )
 
-  def which_one(text: String, options: List[MTQuestionOption]) = a.RadioButtonQuestion { q =>
-    q.budget = 5.00
-    q.text = text
-    q.options = options
-    q.minimum_spawn_policy = UserDefinableSpawnPolicy(0)
-  }
+  def which_one(text: String, options: List[MTQuestionOption]) = radio (
+    budget = 5.00,
+    text = text,
+    options = options,
+    minimum_spawn_policy = UserDefinableSpawnPolicy(0)
+  )
   
   // images:
   val images = Map(
@@ -42,11 +39,8 @@ object banana_question extends App {
   val s3client = init_s3(opts('key), opts('secret), bucketname)
   val s3_urls = scaled.map{ case (name,img) => name -> getTinyURL(store_in_s3(img, s3client, bucketname)) }
   
-  // create options
-  val banana_opts = s3_urls.map { case (name, url) => a.Option(Symbol(name), name, url) }.toList
-
   automan(a) {
-    val outcome = which_one("Which one of these does not belong?", banana_opts)
+    val outcome = which_one("Which one of these does not belong?", s3_urls.toList)
     outcome.answer match {
       case answer: Answer[Symbol] => println("The answer is: " + answer.value)
       case _ => println("An error occurred.")
