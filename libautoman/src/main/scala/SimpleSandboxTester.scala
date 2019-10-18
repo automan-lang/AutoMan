362fe342ae58875ecd3f4c000932bce4e69ebb5a
Daniel W Barowy <dbarowy@cs.williams.edu>
2019-10-18 14:10:20 -0400

    app for run config

---
+++ libautoman/src/main/scala/SimpleSandboxTester.scala
@@ -0,0 +1,37 @@
+import edu.umass.cs.automan.adapters.mturk.DSL._
+import edu.umass.cs.automan.core.policy.aggregation.UserDefinableSpawnPolicy
+
+object SimpleSandboxTester extends App {
+  val opts = Utilities.unsafe_optparse(args, "SimpleSandboxTester")
+
+  implicit val a = mturk (
+    access_key_id = opts('key),
+    secret_access_key = opts('secret),
+    sandbox_mode = opts('sandbox).toBoolean
+  )
+
+  def which_one() = radio (
+    budget = 8.00,
+    text = "Which one of these does not belong?",
+    options = (
+      choice('oscar, "Oscar the Grouch", "http://tinyurl.com/qfwlx56"),
+      choice('kermit, "Kermit the Frog", "http://tinyurl.com/nuwyz3u"),
+      choice('spongebob, "Spongebob Squarepants", "http://tinyurl.com/oj6wzx6"),
+      choice('cookiemonster, "Cookie Monster", "http://tinyurl.com/otb6thl"),
+      choice('thecount, "The Count", "http://tinyurl.com/nfdbyxa")
+    ),
+    minimum_spawn_policy = UserDefinableSpawnPolicy(0)
+  )
+
+  automan(a) {
+    which_one().answer match {
+      case answer: Answer[Symbol] =>
+        println("The answer is: " + answer.value)
+      case lowconf: LowConfidenceAnswer[Symbol] =>
+        println(
+          "You ran out of money. The best answer is \"" +
+            lowconf.value + "\" with a confidence of " + lowconf.confidence
+        )
+    }
+  }
+}
