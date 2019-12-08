736ed7ba70c12f69b0021286822a97f0221fe6e0
emmiehine <emmie.e.hine@gmail.com>
2019-12-08 13:51:56 -0500

    render works too

--- libautoman/src/main/scala/SampleGrammar.scala
+++ libautoman/src/main/scala/SampleGrammar.scala
@@ -143,152 +143,6 @@ class Function(fun: Map[String,String], param: String, capitalize: Boolean) exte
 
 object SampleGrammar {
 
-  /*
-  // Sample a string from the grammar
-  def sample(g: Map[String,Production], startSymbol: String, scope: Scope): Unit = {
-    // find start
-    // sample symbol associated with it
-    // build string by sampling each symbol
-    val samp: Option[Production] = g get startSymbol // get Production associated with symbol from grammar
-    samp match {
-      case Some(samp) => {
-        //println(s"${samp} is a LNT ${samp.isLeafNT()}")
-        samp match {
-          case name: Name => sample(g, name.sample(), scope) // Name becomes start symbol
-          case term: Terminal => {
-            print(term.sample())
-          }
-          case choice: Choices => {
-            if(scope.isBound(startSymbol)){
-              //println(s"${startSymbol} is bound, looking up")
-              print(scope.lookup(startSymbol))
-            } else {
-              throw new Exception(s"Choice ${startSymbol} has not been bound")
-            }
-          }
-          case nonterm: Sequence => {
-            for(n <- nonterm.getList()) {
-              n match {
-                case name: Name => sample(g, name.sample(), scope)
-                case fun: Function => print(fun.runFun(scope.lookup(fun.sample())))
-                case p: Production => {
-                  if(scope.isBound(startSymbol)){
-                    print(scope.lookup(startSymbol))
-                  } else {
-                    print(p.sample())
-                  }
-                }
-              }
-            }
-          }
-          case fun: Function => { // need to look up element in grammar via getParam, find its binding, then use that in runFun
-            if(scope.isBound(fun.sample())){ // TODO: is this right?
-              print(fun.runFun(scope.lookup(fun.sample())))
-            } else {
-              print("Variable not bound")
-            }
-          }
-        }
-      }
-      case None => throw new Exception(s"Symbol ${startSymbol} could not be found")
-    }
-  }
-
-  // bind variables. Doesn't deal with functions; those are handled by Sample
-  def bind(grammar: Map[String, Production], startSymbol: String, scope: Scope): Unit ={
-    val samp: Option[Production] = grammar get startSymbol // get Production associated with symbol from grammar
-    samp match {
-      case Some(samp) => {
-        samp match {
-          case name: Name => bind(grammar, name.sample(), scope) // Name becomes start symbol
-          case choice: Choices => {
-            if(!(scope.isBound(startSymbol))){
-              val binding = choice.sample()
-              scope.assign(startSymbol, binding)
-              //println(scope.toString())
-            }
-          }
-          case nt: Sequence => {
-            for(n <- nt.getList()) {
-              n match {
-                case name: Name => bind(grammar, name.sample(), scope)
-                case p: Production => {}
-              }
-            }
-          }
-          case p: Production => {}
-          }
-        }
-      case None => throw new Exception(s"Symbol ${startSymbol} could not be found")
-      }
-    }
-
-
-  /** Working here  */
-    // Gets an instance of an experiment via an array of ints
-  def getInstance(grammar: Map[String, Production], choiceArr: Option[Array[Range]], scope: Scope, params: Array[Int]): ArrayBuffer[String] = {
-      // walk through grammar and choicearr and match up values with params passed in
-      //assert(grammar.size == params.length)
-
-      var toGet: String = ""
-
-      val init = grammar get "Start"
-      init match {
-        case Some(name) => {
-          toGet = name.sample()
-          //val seq = grammar get name.sample()
-        }
-        case None => {
-          throw new Error("ya done goofed")
-        }
-      }
-    //var gIndex = 0
-    val seq = grammar get toGet
-    var choiceIndex = 0
-    var instance = new ArrayBuffer[String](params.length)
-
-      seq match {
-        case Some(s) => {
-          for(e <- s.asInstanceOf[Sequence].getList()){ //TODO: this feels janky
-            e match {
-              case choice: Choices => { // will this ever trigger?
-                instance += choice.getOptions()(params(choiceIndex)).sample()
-                choiceIndex += 1
-              }
-              case terminal: Terminal => {
-                instance += terminal.sample()
-              }
-              case name: Name => {
-                val choice = grammar get name.sample()
-                choice match { // will a name ever go to anything but a choice?
-                  case Some(prod) => {
-                    prod match {
-                      case choice: Choices => {
-                        instance += choice.getOptions()(params(choiceIndex)).sample() //TODO: make sure array isn't out of bounds
-                        choiceIndex += 1
-                      }
-                    }
-                  }
-                  case None => {
-                    throw new Error("Name is invalid; there should be a choice here.")
-                  }
-                }
-              }
-              case fun: Function => {
-                instance += fun.runFun(scope.lookup(fun.sample()))
-              }
-            }
-          }
-        }
-        case None => {
-          throw new Error("There should be a sequence here")
-        }
-      }
-
-    instance
-    //Array[String]()
-  }
-*/
   // Count the number of options possible in a given grammar
   def count(grammar: Map[String, Production], startSymbol: String, soFar: Int, counted: mutable.HashSet[String]): Int = {
     val samp: Option[Production] = grammar get startSymbol // get Production associated with symbol from grammar
@@ -348,6 +202,55 @@ object SampleGrammar {
             newScope
           }
           //case p: Production => {}
+        }
+      }
+      case None => throw new Exception(s"Symbol ${startSymbol} could not be found")
+    }
+  }
+
+  def render(g: Map[String,Production], startSymbol: String, scope: Scope): Unit = {
+    // find start
+    // sample symbol associated with it
+    // build string by sampling each symbol
+    val samp: Option[Production] = g get startSymbol // get Production associated with symbol from grammar
+    samp match {
+      case Some(samp) => {
+        //println(s"${samp} is a LNT ${samp.isLeafNT()}")
+        samp match {
+          case name: Name => render(g, name.sample(), scope) // Name becomes start symbol
+          case term: Terminal => {
+            print(term.sample())
+          }
+          case choice: Choices => {
+            if(scope.isBound(startSymbol)){
+              //println(s"${startSymbol} is bound, looking up")
+              print(scope.lookup(startSymbol))
+            } else {
+              throw new Exception(s"Choice ${startSymbol} has not been bound")
+            }
+          }
+          case nonterm: Sequence => {
+            for(n <- nonterm.getList()) {
+              n match {
+                case name: Name => render(g, name.sample(), scope)
+                case fun: Function => print(fun.runFun(scope.lookup(fun.sample())))
+                case p: Production => {
+                  if(scope.isBound(startSymbol)){
+                    print(scope.lookup(startSymbol))
+                  } else {
+                    print(p.sample())
+                  }
+                }
+              }
+            }
+          }
+          case fun: Function => { // need to look up element in grammar via getParam, find its binding, then use that in runFun
+            if(scope.isBound(fun.sample())){ // TODO: is this right?
+              print(fun.runFun(scope.lookup(fun.sample())))
+            } else {
+              print("Variable not bound")
+            }
+          }
         }
       }
       case None => throw new Exception(s"Symbol ${startSymbol} could not be found")
@@ -480,7 +383,7 @@ object SampleGrammar {
     //println()
     val scope = bind(Linda, "Start", Array(0,1,3,0,0,0,0,0,0,0), 0)
     for(e <- scope.getBindings()) println(e)
-    //sample(Linda, "Start", lindaScope)
+    render(Linda, "Start", scope)
     println()
     val choiceArr: Option[Array[Range]] = lindaS.toChoiceArr(Linda) // acting on the sequence
     //if(choiceArr.length > 0) {
