1d892412ca8b9096e45afb17eaeb5513dba53b53
emmiehine <emmie.e.hine@gmail.com>
2020-01-06 23:00:12 -0500

    product working, something funky happening with unrank

--- libautoman/src/main/scala/SampleGrammar.scala
+++ libautoman/src/main/scala/SampleGrammar.scala
@@ -270,21 +270,31 @@ object SampleGrammar {
 
   // Product of list l. If l is empty, product is 1
   def product(l: Array[Int]): Int = {
-    if(l.length == 1) 1
-    else l(0) * product(l.slice(1, l.length - 1))
+    var toRet = 1
+    if (l.length != 0) { //todo redundant?
+      //toRet = 1
+      for (e <- l) {
+        toRet *= e
+      }
+    }
+    toRet
+
+//    if(l.length == 0) 1
+//    else l(0) * product(l.slice(1, l.length - 1))
   }
 
   def rank(vals: Array[Int], bases: Array[Int]): Int = {
     var toRet = 0
-    for(i <- 1 to vals.length - 1){
+    for(i <- 0 to vals.length - 1){
       toRet += vals(i)*product(bases.slice(i+1,bases.length - 1))
     }
     toRet
   }
 
+  //todo something funky happening here
   def unrank(rank: Int, bases: Array[Int]): ArrayBuffer[Int] = {
     var toRet: ArrayBuffer[Int] = new ArrayBuffer[Int]()
-    for(i <- 1 to bases.length - 1){
+    for(i <- 0 to bases.length - 1){
       toRet += rank/product(bases.slice(i+1, bases.length - 1))%bases(i) // todo: make sure order of operations ok
     }
     toRet
@@ -442,6 +452,20 @@ object SampleGrammar {
 
     println()
     println("Linda count: "  + count(Linda, "Start", 0, new mutable.HashSet[String]()))
+
+
+    val bases = Array(1,2,3,4)
+    val total = product(bases)
+
+    println(s"total: ${total}")
+
+    for(i <- 0 to total){
+      var values = unrank(i,bases)
+      var r = rank(values.toArray, bases)
+      //assert(i == r)
+      println(s"${values} ${r}")
+    }
+
     //println(s"New count: ${newCount}")
 //    val instance = getInstance(Linda, choiceArr, lindaScope,  Array(0,1,3,0,0,0,0,0,0,0))//,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
 //    println("Classic instance: ")
