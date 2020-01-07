af010ab5550cfbaee60f1c5f35a7d08ddb541f84
emmiehine <emmie.e.hine@gmail.com>
2020-01-07 12:29:28 -0500

    moved ranking code to new file

---
+++ libautoman/src/main/scala/Ranking.scala
@@ -0,0 +1,50 @@
+import scala.collection.mutable.ArrayBuffer
+
+class Ranking {
+
+  // Product of list l. If l is empty, product is 1
+  def product(l: Array[Int]): Int = {
+    var toRet = 1
+    if (l.length != 0) { //todo redundant?
+      //toRet = 1
+      for (e <- l) {
+        toRet *= e
+      }
+    }
+    toRet
+
+    //    if(l.length == 0) 1
+    //    else l(0) * product(l.slice(1, l.length - 1))
+  }
+
+  def rank(vals: Array[Int], bases: Array[Int]): Int = {
+    var toRet = 0
+    for(i <- 0 to vals.length - 1){
+      toRet += vals(i)*product(bases.slice(i+1,bases.length - 1))
+    }
+    toRet
+  }
+
+  //todo something funky happening here
+  def unrank(rank: Int, bases: Array[Int]): Array[Int] = {
+    var toRet: ArrayBuffer[Int] = new ArrayBuffer[Int]()
+    for(i <- 0 to bases.length - 1){
+      toRet += rank/product(bases.slice(i+1, bases.length - 1))%bases(i) // todo: make sure order of operations ok
+    }
+    toRet.toArray
+  }
+
+  def main(args: Array[String]): Unit = {
+    val bases = Array(1,2,3,4)
+    val total = product(bases)
+
+    println(s"total: ${total}")
+
+    for(i <- 0 to total){
+      var values = unrank(i,bases)
+      var r = rank(values, bases)
+      //assert(i == r)
+      println(s"${values} ${r}")
+    }
+  }
+}
--- libautoman/src/main/scala/SampleGrammar.scala
+++ libautoman/src/main/scala/SampleGrammar.scala
@@ -268,38 +268,6 @@ object SampleGrammar {
     }
   }
 
-  // Product of list l. If l is empty, product is 1
-  def product(l: Array[Int]): Int = {
-    var toRet = 1
-    if (l.length != 0) { //todo redundant?
-      //toRet = 1
-      for (e <- l) {
-        toRet *= e
-      }
-    }
-    toRet
-
-//    if(l.length == 0) 1
-//    else l(0) * product(l.slice(1, l.length - 1))
-  }
-
-  def rank(vals: Array[Int], bases: Array[Int]): Int = {
-    var toRet = 0
-    for(i <- 0 to vals.length - 1){
-      toRet += vals(i)*product(bases.slice(i+1,bases.length - 1))
-    }
-    toRet
-  }
-
-  //todo something funky happening here
-  def unrank(rank: Int, bases: Array[Int]): ArrayBuffer[Int] = {
-    var toRet: ArrayBuffer[Int] = new ArrayBuffer[Int]()
-    for(i <- 0 to bases.length - 1){
-      toRet += rank/product(bases.slice(i+1, bases.length - 1))%bases(i) // todo: make sure order of operations ok
-    }
-    toRet
-  }
-
   def main(args: Array[String]): Unit = {
 
     val pronouns = Map[String, String](
@@ -452,19 +420,6 @@ object SampleGrammar {
 
     println()
     println("Linda count: "  + count(Linda, "Start", 0, new mutable.HashSet[String]()))
-
-
-    val bases = Array(1,2,3,4)
-    val total = product(bases)
-
-    println(s"total: ${total}")
-
-    for(i <- 0 to total){
-      var values = unrank(i,bases)
-      var r = rank(values.toArray, bases)
-      //assert(i == r)
-      println(s"${values} ${r}")
-    }
 
     //println(s"New count: ${newCount}")
 //    val instance = getInstance(Linda, choiceArr, lindaScope,  Array(0,1,3,0,0,0,0,0,0,0))//,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
