d88e5dde4c3b96e2b7ce732f0a9d411783087689
Ye Shu <hello@shuye.dev>
2022-05-13 11:18:27 -0400

    Add element randomization

--- libautoman/src/main/scala/org/automanlang/adapters/mturk/question/MTFakeSurvey.scala
+++ libautoman/src/main/scala/org/automanlang/adapters/mturk/question/MTFakeSurvey.scala
@@ -125,8 +125,11 @@ class MTFakeSurvey extends FakeSurvey with MTurkQuestion {
             <body>
               <script src="https://assets.crowd.aws/crowd-html-elements.js"></script>
               <crowd-form>
+                <div id="container">
                 ${toQuestionHTML(randomize)}
+                </div>
               </crowd-form>
+              ${randomizeScript()}
             </body>
           </html>
         ]]>
@@ -134,6 +137,93 @@ class MTFakeSurvey extends FakeSurvey with MTurkQuestion {
       <FrameHeight>0</FrameHeight>
     </HTMLQuestion>
     """
+  }
+
+  // https://stackoverflow.com/questions/23215162/assignmentid-not-visible-in-mturk-accept-url
+  //
+  def randomizeScript(): String = {
+    s"""
+       |<script>
+       |const shuffleNodes = () => {
+       |  console.log("[DEBUG] Start shuffling elements");
+       |  const list = document.getElementById("container");
+       |
+       |  function turkGetParam( name ) {
+       |    var regexS = "[\\?&]"+name+"=([^&#]*)";
+       |    var regex = new RegExp( regexS );
+       |    var tmpURL = window.location.href;
+       |    var results = regex.exec( tmpURL );
+       |    if( results == null ) {
+       |        return "";
+       |    } else {
+       |        return results[1];
+       |    }
+       |  }
+       |
+       |  let seed = turkGetParam('workerId');
+       |  console.log("[DEBUG] workerId" + seed);
+       |
+       |  // a custom seeded PRNG function generator.
+       |  // Taken from https://stackoverflow.com/a/47593316/6604166
+       |  const xmur3 = (str) => {
+       |    for (var i = 0, h = 1779033703 ^ str.length; i < str.length; i++) {
+       |      h = Math.imul(h ^ str.charCodeAt(i), 3432918353);
+       |      h = h << 13 | h >>> 19;
+       |    } return function () {
+       |      h = Math.imul(h ^ (h >>> 16), 2246822507);
+       |      h = Math.imul(h ^ (h >>> 13), 3266489909);
+       |      return (h ^= h >>> 16) >>> 0;
+       |    }
+       |  }
+       |
+       |  const sfc32 = (a, b, c, d) => {
+       |    return function () {
+       |      a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0;
+       |      var t = (a + b) | 0;
+       |      a = b ^ b >>> 9;
+       |      b = c + (c << 3) | 0;
+       |      c = (c << 21 | c >>> 11);
+       |      d = d + 1 | 0;
+       |      t = t + d | 0;
+       |      c = c + t | 0;
+       |      return (t >>> 0) / 4294967296;
+       |    }
+       |  }
+       |
+       |  const shuffle = (items, prng) => {
+       |    let cached = items.slice(0);
+       |    let i = cached.length;
+       |    let temp, rand;
+       |    while (--i) {
+       |      rand = Math.floor(i * prng());
+       |      temp = cached[rand];
+       |      cached[rand] = cached[i];
+       |      cached[i] = temp;
+       |    }
+       |    return cached;
+       |  }
+       |
+       |  // Create xmur3 state:
+       |  const seeder = xmur3(seed);
+       |  // Output four 32-bit hashes to provide the seed for sfc32.
+       |  const rand = sfc32(seeder(), seeder(), seeder(), seeder());
+       |
+       |  // shuffle
+       |  let nodes = list.children, i = 0;
+       |  nodes = Array.prototype.slice.call(nodes);
+       |  nodes = shuffle(nodes, rand);
+       |
+       |  // writeback
+       |  while (i < nodes.length) {
+       |    list.appendChild(nodes[i]);
+       |    ++i;
+       |  }
+       |  console.log("[DEBUG] Finish shuffling elements")
+       |}
+       |
+       |document.addEventListener('DOMContentLoaded', shuffleNodes, false);
+       |</script>
+       |""".stripMargin
   }
 
   override protected[mturk] def toQuestionHTML(randomize: Boolean): String = {
