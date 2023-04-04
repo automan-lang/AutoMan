a9c7981ff5eb5bc660d77084f1acfeb1c7d940d9
Daniel W. Barowy <dbarowy@cs.williams.edu>
2023-04-03 20:30:27 -0400

    A somewhat unglamorous way of providing qualifications for surveys, but it works.

--- libautoman/src/main/scala/org/automanlang/adapters/mturk/DSL.scala
+++ libautoman/src/main/scala/org/automanlang/adapters/mturk/DSL.scala
@@ -1,13 +1,16 @@
 package org.automanlang.adapters.mturk
 
+import com.amazonaws.services.mturk.model.QualificationRequirement
+
 import scala.language.implicitConversions
-import org.automanlang.core.answer._
+import org.automanlang.adapters.mturk.question.MTFakeSurvey
 import org.automanlang.adapters.mturk.mock.MockSetup
-import org.automanlang.adapters.mturk.question.MTQuestionOption
-import org.automanlang.core.AutomanAdapter
+import org.automanlang.core.{AutomanAdapter, MagicNumbers}
 import org.automanlang.core.logging.LogConfig.LogConfig
 import org.automanlang.core.logging.{LogLevel, LogLevelInfo}
 import org.automanlang.core.policy.aggregation.MinimumSpawnPolicy
+import org.automanlang.core.question.{FakeSurvey, Question}
+import scala.collection.immutable.ListMap
 
 object DSL extends org.automanlang.core.DSL {
   type MTQuestionOption = org.automanlang.adapters.mturk.question.MTQuestionOption
@@ -35,6 +38,73 @@ object DSL extends org.automanlang.core.DSL {
     }
 
     MTurkAdapter(initf)
+  }
+
+  def SurveyWithQuals[A <: MTurkAdapter, O](
+                                      questions: List[Question],
+                                      budget: BigDecimal = MagicNumbers.DefaultBudget,
+                                      sample_size: Int = MagicNumbers.DefaultSampleSizeForDistrib,
+                                      dont_reject: Boolean = true,
+                                      dry_run: Boolean = false,
+                                      initial_worker_timeout_in_s: Int = MagicNumbers.InitialWorkerTimeoutInS,
+                                      minimum_spawn_policy: MinimumSpawnPolicy = null,
+                                      pay_all_on_failure: Boolean = true,
+                                      question_timeout_multiplier: Double = MagicNumbers.QuestionTimeoutMultiplier,
+                                      text: String,
+                                      title: String = null,
+                                      csv_output: String = null,
+                                      wage: BigDecimal = MagicNumbers.USFederalMinimumWage,
+                                      cohen_d_threshold: Double = 12,
+                                      noise_percentage: Double = 0.2,
+                                      words_candidates: ListMap[String, Array[String]] = ListMap(),
+                                      functions: ListMap[String, (String, Map[String, String])] = ListMap(),
+                                      qualifications: List[QualificationRequirement]
+                                    )(implicit a: MTurkAdapter): FakeSurvey#O = {
+    def initf[Q <: FakeSurvey](q2: Q): Unit = {
+      val q = q2.asInstanceOf[MTFakeSurvey]
+      // mandatory parameters
+      q.text = text
+      q.questions = questions
+
+      // mandatory parameters with sane defaults
+      q.budget = budget
+      q.dont_reject = dont_reject
+      q.dry_run = dry_run
+      q.initial_worker_timeout_in_s = initial_worker_timeout_in_s
+      q.pay_all_on_failure = pay_all_on_failure
+      q.question_timeout_multiplier = question_timeout_multiplier
+      q.sample_size = sample_size
+      q.wage = wage
+      q.qualifications = qualifications
+
+      // optional parameters
+      //      if (image_alt_text != null) {
+      //        q.image_alt_text = image_alt_text
+      //      }
+      //      if (image_url != null) {
+      //        q.image_url = image_url
+      //      }
+      if (title != null) {
+        q.title = title
+      }
+      //      if (mock_answers != null) {
+      //        q.mock_answers = mock_answers
+      //      }
+      if (minimum_spawn_policy != null) {
+        q.minimum_spawn_policy = minimum_spawn_policy
+      }
+
+      if (csv_output != null) {
+        q.csv_output = csv_output
+      }
+
+      q.d_threshold = cohen_d_threshold
+      q.noise_percentage = noise_percentage
+      q.words_candidates = words_candidates
+      q.functions = functions
+    }
+
+    a.Survey(initf)
   }
 
   def choice(key: Symbol, text: String)(implicit mt: MTurkAdapter) : MTQuestionOption = {
